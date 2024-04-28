13c5143928c6ca74af5d008815063c44e83d434c
dema1537 <dw1537@york.ac.uk>
2024-04-28 16:11:01 +0100

    Finished all asset tests, including refactoring of Playing.java so the assets are able to be called as variables rather than strings

--- core/src/io/github/uoyeng1g6/tests/AssetTests.java
+++ core/src/io/github/uoyeng1g6/tests/AssetTests.java
@@ -1,29 +1,23 @@
 package io.github.uoyeng1g6.tests;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import io.github.uoyeng1g6.HeslingtonHustle;
 
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.logging.FileHandler;
-
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 
 
 @RunWith(GdxTestRunner.class)
 public class AssetTests {
-
     @Test
     public void t_InteractionIconExists() {
 
         assertNotNull("The asset for interactions exists", Gdx.files.internal(HeslingtonHustle.interactionAsset));
     }
-
     @Test
     public void t_PlayerIconExists() {
 
