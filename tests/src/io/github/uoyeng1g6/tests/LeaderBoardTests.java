0f6e45b33a38b56b33c0dc554d7ba7bc794e4dd5
James Lawson <67314546+JamesLawson01@users.noreply.github.com>
2024-05-18 23:44:31 +0100

    Added extra leaderboard tests

--- tests/src/io/github/uoyeng1g6/tests/LeaderBoardTests.java
+++ tests/src/io/github/uoyeng1g6/tests/LeaderBoardTests.java
@@ -1,25 +1,15 @@
 package io.github.uoyeng1g6.tests;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import io.github.uoyeng1g6.HeslingtonHustle;
-import io.github.uoyeng1g6.models.GameState;
-import io.github.uoyeng1g6.screens.Playing;
 import io.github.uoyeng1g6.utils.LeaderboardManager;
 import io.github.uoyeng1g6.utils.PlayerScore;
-import org.junit.Before;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
 
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(GdxTestRunner.class)
 public class LeaderBoardTests {
@@ -32,4 +22,51 @@ public class LeaderBoardTests {
         List<PlayerScore> scores = lm.getTopTenScores();
         assertTrue(scores.contains(score));
     }
+
+    @Test
+    public void testHighestScore() {
+        LeaderboardManager lm = LeaderboardManager.getInstance();
+        for (int i=0; i<10; i++) {
+            lm.addScore("P" + i+1, 50-i);
+        }
+        PlayerScore p11 = new PlayerScore("P11", 100);
+        lm.addScore(p11);
+        List<PlayerScore> top10 = lm.getTopTenScores();
+        assertEquals(top10.get(0), p11);
+    }
+
+    @Test
+    public void testLowScore() {
+        LeaderboardManager lm = LeaderboardManager.getInstance();
+        for (int i=0; i<10; i++) {
+            lm.addScore("P" + i+1, 50-i);
+        }
+        PlayerScore p11 = new PlayerScore("P11", 0);
+        lm.addScore(p11);
+        List<PlayerScore> top10 = lm.getTopTenScores();
+        assertFalse(top10.contains(p11));
+    }
+
+    @Test
+    public void test10Scores() {
+        LeaderboardManager lm = LeaderboardManager.getInstance();
+        for (int i=0; i<10; i++) {
+            lm.addScore("P" + i+1, 50-i);
+        }
+        PlayerScore p11 = new PlayerScore("P11", 70);
+        lm.addScore(p11);
+        List<PlayerScore> top10 = lm.getTopTenScores();
+        assertEquals(10, top10.size());
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        LeaderboardManager.clearScores();
+    }
+
+    @After
+    public void cleanUp() {
+        LeaderboardManager.clearScores();
+    }
+
 }
